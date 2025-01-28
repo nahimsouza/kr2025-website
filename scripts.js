c84987bae38ab0080e1d924bb2d8e9cce261cdcd
Nahim Alves de Souza <nahimsouza@outlook.com>
2025-01-28 15:42:54 -0300

    First version of KR 2025 website.

--- scripts.js
+++ scripts.js
@@ -9,12 +9,19 @@ function loadComponents() {
     <nav>
         <a href="index.html">
             <div class="logo">
-                <img src="images/the-amazing-conf-logo-blue-transparent.png" alt="Conference Logo" height="60px">
+                <img src="images/logo_kr_blue_transparent.png" alt="Conference Logo" height="60px">
             </div>
         </a>
         <ul class="main__ul">
             <li><a href="index.html">Home</a></li>
             <li><a href="dates.html">Important Dates</a></li>
+            <li><a>Venue/Local</a>
+                <ul class="dropdown">
+                    <li><a href="gallery.html">Gallery</a></li>
+                </ul>
+            </li>
+            <li><a href="guidelines.html">Submissions</a></li>
+            <!--
             <li><a href="#">Calls</a>
                 <ul class="dropdown">
                     <li><a href="#">Track 1</a></li>
@@ -22,18 +29,11 @@ function loadComponents() {
                     <li><a href="#">Track 3</a></li>
                 </ul>
             </li>
-            <li><a href="#">Submissions</a></li>
-            <li><a href="#">Venue/Local</a>
-            <ul class="dropdown">
-                <li><a href="gallery.html">Gallery</a></li>
-                <li><a href="#">Item 2</a></li>
-                <li><a href="#">Item 3</a></li>
-            </ul>
-            </li>
             <li><a href="#">Organization</a></li>
             <li><a href="#">Program</a></li>
             <li><a href="#">Sponsoring</a></li>
             <li><a href="#" class="registration-btn">Registration</a></li>
+            -->
         </ul>
     </nav>
     <div id='menu' class='box-icon'><i class='bx bx-menu'></i></div>
@@ -42,7 +42,7 @@ function loadComponents() {
 
     footer = `
     <footer>
-        <p>© Copyright 2025. Conferece Website Template - Designed by <a href="https://github.com/nahimsouza/conference-website/">nahimsouza</a>. MIT License.</p>
+        <p>© Copyright - KR2025 Website was based on <a href="https://github.com/nahimsouza/conference-website/">Conference Website Template</a>. MIT License.</p>
     </footer>
     `
 
